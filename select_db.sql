66e1a153f8df7ca2f601b912b3b61c55b677bfce
ligont <129254809+ligont@users.noreply.github.com>
2023-05-09 14:06:05 +0700

    Update select_db.sql
    Очередные исправление

--- select_db.sql
+++ select_db.sql
@@ -1,24 +1,25 @@
 /*название и год выхода альбомов, вышедших в 2018 году;*/
-SELECT name, year_of  FROM album
-where year_of = 2018;
+SELECT name AS album_name, year_of AS release_year FROM album
+WHERE year_of = 2018;
 
 /*название и продолжительность самого длительного трека;*/
-SELECT name, duration FROM songs
-order by duration desc 
-limit 1;
+SELECT name AS song_name, duration AS song_length FROM songs
+ORDER BY duration DESC 
+LIMIT 1;
 
 /*название треков, продолжительность которых не менее 3,5 минуты;*/
-SELECT name  FROM songs
-where duration >= 3.5*60;
+SELECT name AS song_name FROM songs
+WHERE duration >= 3.5*60;
 
 /*названия сборников, вышедших в период с 2018 по 2020 год включительно;*/
-SELECT name, year_of  FROM collection
-where year_of BETWEEN 2018 and 2021;
+SELECT name AS collection_name, year_of AS release_year FROM collection
+WHERE year_of BETWEEN 2018 AND 2020;
 
 /*исполнители, чье имя состоит из 1 слова;*/
-SELECT name  FROM bands
-where name not like '% %';
+SELECT name AS band_name FROM bands
+WHERE name NOT LIKE '% %';
 
 /*название треков, которые содержат слово "мой"/"my".*/
 SELECT name FROM songs
-WHERE LOWER(name) like '%my%' or LOWER(name) like '%мой%';
+WHERE name ILIKE 'my %' OR name ILIKE '% my' OR name ILIKE '% my %' OR name = 'my'
+OR name ILIKE 'мой %' OR name ILIKE '% мой' OR name ILIKE '% мой %' OR name = 'мой';
